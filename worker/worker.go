945ec56b2710465033637ab03e059f682a2dc9a2
Alex Martishin <asmartishin@gmail.com>
2025-06-21 15:44:09 +0200

    Added worker skeleton

--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,32 @@
 package worker
+
+import (
+	"fmt"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"github.com/martishin/orchestrator-in-go/task"
+)
+
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]*task.Task
+	TaskCount int
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("Collecting stats")
+}
+
+func (w *Worker) RunTask() {
+	fmt.Println("Running task")
+}
+
+func (w *Worker) StartTask() {
+	fmt.Println("Starting task")
+}
+
+func (w *Worker) StopTask() {
+	fmt.Println("Stopping task")
+}
